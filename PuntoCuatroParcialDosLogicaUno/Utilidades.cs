97a157c2e1cda01fe39abb0a93fd3a24ebfa70e6
Sofito-code <65092315+Sofito-code@users.noreply.github.com>
2023-09-29 18:32:44 -0500

    Primer commit

---
+++ PuntoCuatroParcialDosLogicaUno/Utilidades.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PuntoCuatroParcialDosLogicaUno
+{
+    internal class Utilidades
+    {
+        public bool esEntero(float numero)
+        {
+            if(numero%1 != 0)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public bool esNumero(String numero)
+        {
+            float num = 0;
+            if (float.TryParse(numero,out num))
+            {
+                return true;   
+            }
+            return false;
+        }
+    }
+}
