cd757a1557c937636b9ea3089c46ae345efde947
Sofito-code <65092315+Sofito-code@users.noreply.github.com>
2023-09-29 20:13:56 -0500

    Lógica de juego terminada

--- PuntoCuatroParcialDosLogicaUno/Program.cs
+++ PuntoCuatroParcialDosLogicaUno/Program.cs
@@ -6,11 +6,107 @@ using System.Threading.Tasks;
 
 namespace PuntoCuatroParcialDosLogicaUno
 {
+
     internal class Program
     {
         static void Main(string[] args)
         {
-            Random rnd = new Random();
+            Utilidades utilidades = new Utilidades(); 
+            int numeroAleatorio = utilidades.generarNumeroAleatorio(-99999999, 99999999);
+            Console.WriteLine("Juego La Queso");
+            bool jugar = true;
+            int ronda = 1;
+            while (jugar)
+            {
+                Console.WriteLine("Ronda: " + ronda);
+                String numeroIngresado = utilidades.mostrar("Ingrese un número: ");
+                if (utilidades.esNumero(numeroIngresado))
+                {                    
+                    if (utilidades.esEntero(numeroIngresado))
+                    {
+                        int numeroUsuario = int.Parse(numeroIngresado);
+                        if (numeroUsuario == numeroAleatorio)
+                        {
+                            Console.WriteLine("Felicitaciones");
+                            jugar = false;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Sigue intentando");
+                            switch (ronda)
+                            {
+                                case 2:
+                                    Console.WriteLine("Pista!");
+                                    if (numeroAleatorio > 0)
+                                    {
+                                        Console.WriteLine("El número es mayor a cero");
+                                    }
+                                    else
+                                    {
+                                        Console.WriteLine("El número es menor a cero");
+                                    }
+                                    ronda++;
+                                    break;
+                                case 4:
+                                    if (numeroAleatorio%2 == 0)
+                                    {
+                                        Console.WriteLine("El número es par");
+                                    }
+                                    else
+                                    {
+                                        Console.WriteLine("El número es impar");
+                                    }
+                                    ronda++;
+                                    break;
+                                case 6:
+                                    if (numeroAleatorio > 0)
+                                    {
+                                        int cantidadDigitos = numeroAleatorio.ToString().Length;
+                                        Console.WriteLine($"El número que debes adivinar tiene {cantidadDigitos} dígitos");
+                                    }
+                                    else
+                                    {
+                                        int cantidadDigitos = numeroAleatorio.ToString().Length - 1;
+                                        Console.WriteLine($"El número que debes adivinar tiene {cantidadDigitos} dígitos");
+                                    }
+                                    ronda++;
+                                    break;
+                                case 8:
+                                    int iter = 1;
+                                    Console.WriteLine("Los divisores exactos del número son:");
+                                    while(iter < numeroAleatorio)
+                                    {
+                                        if(numeroAleatorio%iter == 0)
+                                        {
+                                            Console.WriteLine(iter);
+                                        }
+                                        iter++;
+                                    }
+                                    ronda++;
+                                    break;
+                                case 10:
+                                    jugar = false;
+                                    Console.WriteLine("El número era" + numeroAleatorio);
+                                    break;
+                                default:
+                                    ronda++;
+                                    break;
+                            }
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Solo se admiten números enteros, siga intentando");
+                        
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Solo se admiten números, siga intentando");
+                }
+                    
+            }
+            
         }
     }
 }
--- PuntoCuatroParcialDosLogicaUno/Utilidades.cs
+++ PuntoCuatroParcialDosLogicaUno/Utilidades.cs
@@ -8,23 +8,39 @@ namespace PuntoCuatroParcialDosLogicaUno
 {
     internal class Utilidades
     {
-        public bool esEntero(float numero)
+        public bool esEntero(String pNumero)
         {
-            if(numero%1 != 0)
+            int numero = 0;
+            bool bandera = int.TryParse(pNumero, out numero);
+            if(bandera)
             {
-                return false;
+                return true;
             }
-            return true;
+            return false;
         }
 
         public bool esNumero(String numero)
         {
             float num = 0;
-            if (float.TryParse(numero,out num))
+            bool bandera=float.TryParse(numero, out num);
+            if (bandera)
             {
                 return true;   
             }
             return false;
+        }
+
+        public String mostrar(String mensaje)
+        {
+            Console.Write(mensaje);
+            return Console.ReadLine();
+        }
+
+        public int generarNumeroAleatorio(int inicio, int fin)
+        {
+            Random rnd = new Random();
+            int numeroGenerado = rnd.Next(inicio, fin);
+            return numeroGenerado;
         }
     }
 }
